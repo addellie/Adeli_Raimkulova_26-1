8d5e8835a76fba43fad6d39d5e239c4cac908672
addellie <adeli.raimkulova@mail.ru>
2023-04-10 00:15:34 +0600

    hw 7

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import HttpResponse, render, redirect
 from datetime import date
 from posts.models import Product, Comment
 from posts.forms import PostCreateForm, CommentCreateForm
-
+from posts.constants import PAGINATION_LIMIT
 
 # Create your views here.
 
@@ -29,11 +29,28 @@ def main_view(request):
 
 def products_view(request):
     if request.method == 'GET':
-        products = Product.objects.all()
+        products = Product.objects.all().order_by('rate', 'price')
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+
+        """starts_with, ends_with, icontains"""
+
+        """and | or == |"""
+
+        if search:
+            products = products.filter(name__icontains=search) | products.filter(description__icontains=search)
+
+        max_page = products.__len__() / PAGINATION_LIMIT
+        max_page = round(max_page) + 1 if round(max_page) < max_page else round(max_page)
+
+        """posts splice"""
+        products = products[PAGINATION_LIMIT * (page-1):PAGINATION_LIMIT * page]
+
 
         context = {
             'products': products,
-            'users': request.user
+            'users': request.user,
+            'page': range(1, max_page+1)
         }
 
         return render(request, 'products/products.html', context=context)
